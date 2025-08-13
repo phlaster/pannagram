228d92b198ea799fbd51c9a6b262e02bdddb2d17
iganna <igolkinaanna11@gmail.com>
2025-08-13 13:30:28 +0200

    test

--- inst/analys/analys_04_snp_dist.R
+++ inst/analys/analys_04_snp_dist.R
@@ -68,7 +68,11 @@ h = hclust(as.dist(dist.mx))
 dist.mx = dist.mx[h$order,h$order]
 p = heatplot(dist.mx)
 
-invisible(suppressMessages(savePDF(h, path=path.figures, name=paste0(file.pi, '_dendro'), width = 7, height = 7)))
+invisible(suppressMessages(
+  pdf(file.path(path.figures, paste0(file.pi, "_dendro.pdf")), width = 7, height = 7)
+  plot(as.dendrogram(h)) 
+  dev.off()
+))
 savePDF(p, path=path.figures, name=paste0(file.pi, '_dist'), width = 7, height = 7)
 
 saveRDS(dist.mx, paste0(path.snp, file.pi, '_dist.rds'))
