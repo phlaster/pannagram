c97f728037d5b60dbaece948620ac434133804ec
iganna <igolkinaanna11@gmail.com>
2025-08-19 17:31:22 +0200

    features for ORF simsearch

--- inst/features.sh
+++ inst/features.sh
@@ -238,9 +238,8 @@ if [ "$run_sv_sim_prot" = true ]; then # -sv_sim_prot
 
     if [ ! -f "${set_file_prot}" ]; then
         pokaz_error "File with proteins does not exist, provide an existing file."
-    fi
-
-    if [ -f "${path_sv}/sv_big_orfs.fasta" ]; then
+        exit 1
+    elif [ -f "${path_sv}/sv_big_orfs.fasta" ]; then
         pokaz_stage "BLAST on proteins..."
 
         path_simsearch_out="${path_sv}.simsearch/"
